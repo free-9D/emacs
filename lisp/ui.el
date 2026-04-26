ddacdb5dc5f4e0a5439f98a10a8135822b7a6704
free-9D <2762397690@qq.com>
2026-04-25 20:07:41 -0400

    add which-key-mode

--- lisp/ui.el
+++ lisp/ui.el
@@ -18,7 +18,23 @@
   ;; 在 Minibuffer 选项右侧显示文件大小、权限、行号等元数据
   (marginalia-mode))
 
-;; --- 3. 侧边栏文件树 (Treemacs) ---
+;; --- 3. 快捷键提示 (Which-key) ---
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode)
+  :config
+  ;; 设置弹出等待时间（秒），0.5 秒比较适合终端用户
+  (setq which-key-idle-delay 0.5)
+  
+  ;; 设置弹出位置，终端建议放在底部 (side-window-bottom)
+  (setq which-key-popup-type 'side-window)
+  
+  ;; 针对终端环境的视觉优化：
+  ;; 如果终端窗口较小，可以限制最大高度
+  (setq which-key-side-window-max-height 0.25))
+
+;; --- 4. 侧边栏文件树 (Treemacs) ---
 (use-package treemacs
   :ensure t
   :bind
